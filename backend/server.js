f3933c54fa511f1c2c8f15fc0e1b5f6ab0534289
saitejagovikar <saiteja51743@gmail.com>
2025-11-18 22:19:31 +0530

    Update CORS configuration and API base URL for production

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   'http://localhost:3000',
   'http://127.0.0.1:3000',
-  'https://companies-directory.vercel.app' // Add your Vercel URL here after deployment
+  'https://companies-directory-ochre.vercel.app/' // Add your Vercel URL here after deployment
 ];
 
 const corsOptions = {
