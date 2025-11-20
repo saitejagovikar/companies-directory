91fab592a064a633a49ecc77939b3e896c629afa
saitejagovikar <saiteja51743@gmail.com>
2025-11-20 12:57:30 +0530

    Update API_URL for production and development

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,8 @@
 // API Configuration
 const isProduction = process.env.NODE_ENV === 'production';
 
-// API Configuration (Only Render)
-export const API_URL = 'https://companies-directory-39z3.onrender.com';
+export const API_URL = isProduction
+  ? 'https://companies-directory-39z3.onrender.com'
+  : 'http://localhost:5003';
 
 export default { API_URL };
